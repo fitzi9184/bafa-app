e6e105bafa6377cfdef144dca189a15d6aaa7fb2
fitzi9184 <fitzi9184@googlemail.com>
2026-06-28 23:02:22 +0200

    Create sendMail.js

---
+++ netlify/functions/sendMail.js
@@ -0,0 +1,71 @@
+const sgMail = require('@sendgrid/mail');
+
+exports.handler = async function(event) {
+
+  try {
+
+    const data = JSON.parse(event.body);
+
+    // API Key aus Netlify
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const message = `
+NEUER BAFA ANTRAG
+
+-------------------------
+Maßnahme:
+${data.massnahme}
+
+-------------------------
+Kunde:
+${data.vorname} ${data.nachname}
+
+E-Mail:
+${data.email}
+
+Telefon:
+${data.telefon}
+
+-------------------------
+Adresse:
+${data.adresse.strasse} ${data.adresse.hausnummer}
+${data.adresse.plz} ${data.adresse.ort}
+
+-------------------------
+Investitionsobjekt:
+${data.investAdresse.strasse} ${data.investAdresse.hausnummer}
+${data.investAdresse.plz} ${data.investAdresse.ort}
+
+-------------------------
+Gebäude:
+Typ: ${data.gebaeude.typ}
+Baujahr: ${data.gebaeude.baujahr}
+
+Wohneinheiten:
+${data.gebaeude.wohneinheiten}
+
+Betroffene WE:
+${data.gebaeude.betroffen}
+`;
+
+    await sgMail.send({
+      to: "DEINE_EMAIL@HIER.EINTRAGEN",
+      from: "noreply@deine-domain.de",
+      subject: "Neuer BAFA Kunde",
+      text: message
+    });
+
+    return {
+      statusCode: 200,
+      body: "Email erfolgreich gesendet"
+    };
+
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: "Fehler beim Senden"
+    };
+  }
+};
